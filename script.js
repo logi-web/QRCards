62fc022827e11ad3cf2200f06f4f8493a796be12
Louis Giron <louisgiron@gmail.com>
2025-02-26 21:03:37 +0000

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ function closeModal(qrId) {
     document.getElementById(qrId + '-modal').classList.remove('active');
     document.body.style.overflow = 'auto';
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    document.querySelector('body').classList.add('loaded');
+  }, 500); // delay 
+});
